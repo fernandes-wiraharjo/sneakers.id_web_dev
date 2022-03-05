51764970e35cff2d3442e9d245474bd0aae536a9
aldyHelix <aldy.satria07@gmail.com>
2022-03-05 14:46:25 +0700

    update tag

--- Modules/Tag/Resources/views/index.blade.php
+++ Modules/Tag/Resources/views/index.blade.php
@@ -18,7 +18,7 @@
         <!--end::Wrapper-->
         @endcan
     </x-slot>
-    <<!--begin::Card-->
+    <!--begin::Card-->
     <div class="card">
         <!--begin::Card body-->
         <div class="card-body pt-6">

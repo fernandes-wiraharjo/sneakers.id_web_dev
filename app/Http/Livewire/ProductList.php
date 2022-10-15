1a4a632354d87df92b0282527383901a50ea8d6e
aldyHelix <aldysatria07@gmail.com>
2022-10-15 08:04:03 +0700

    remove description filter

--- app/Http/Livewire/ProductList.php
+++ app/Http/Livewire/ProductList.php
@@ -147,8 +147,7 @@ class ProductList extends Component
 
         $products = $productRepository->getProductWhere()
                                 ->when($this->search, function ($query, $search){
-                                    return $query->where('product_name', 'LIKE', '%'.$search.'%')
-                                        ->orWhere('description', 'LIKE', '%'.$search.'%');
+                                    return $query->where('product_name', 'LIKE', '%'.$search.'%');
                                 })
                                 ->when($this->brand, function ($query, $brands){
                                     return $query->whereHas('detail', function ($q) use ($brands){

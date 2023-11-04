c1479b39b3bc5d824d9e5046dc74534250a5e256
Aldy Satria Gumilar <aldy.satria07@gmail.com>
2023-11-04 20:32:11 +0700

    style: :lipstick: style size selector button

--- resources/views/store-theme/_partials/_bottom_scripts.blade.php
+++ resources/views/store-theme/_partials/_bottom_scripts.blade.php
@@ -1,20 +1,25 @@
+<script src="http://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+
 <script type="text/javascript">
-    $('.Product__SlideshowNavImage').click(function(){
-        $('.Product__SlideItem').removeClass('is-selected');
-        $('.Product__SlideItem').css('left', '100%');
-        $('.Product__SlideItem').eq($(this).index()).addClass('is-selected');
-        $('.Product__SlideItem').eq($(this).index()).css('left', '0%');
-        $('.Product__SlideshowNavImage').removeClass('is-selected');
-        $('.Product__SlideshowNavImage').eq($(this).index()).addClass('is-selected');
-        $('.flickity-page-dots .dot').removeClass('is-selected')
-        $('.flickity-page-dots .dot').eq($(this).index()).addClass('is-selected');
-    });
+    $(document).ready(function() {
+        $('.Product__SlideshowNavImage').click(function(){
+            $('.Product__SlideItem').removeClass('is-selected');
+            $('.Product__SlideItem').css('left', '100%');
+            $('.Product__SlideItem').eq($(this).index()).addClass('is-selected');
+            $('.Product__SlideItem').eq($(this).index()).css('left', '0%');
+            $('.Product__SlideshowNavImage').removeClass('is-selected');
+            $('.Product__SlideshowNavImage').eq($(this).index()).addClass('is-selected');
+            $('.flickity-page-dots .dot').removeClass('is-selected')
+            $('.flickity-page-dots .dot').eq($(this).index()).addClass('is-selected');
+        });
 
-    $('.Popover__Value').click(function(){
-        $('html').removeClass('no-scroll');
-    });
+        $('.Popover__Value').click(function(){
+            $('html').removeClass('no-scroll');
+        });
 
-    var product_variants_removed = [];
+        var product_variants_removed = [];
+    });
 </script>
 <script src="https://use.fontawesome.com/eeb42b6d4d.js"></script>
 

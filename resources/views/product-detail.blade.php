d72156da9164e0f4ab0d863098d87a5c64fff450
Aldy Satria Gumilar <aldy.satria07@gmail.com>
2023-11-18 22:43:59 +0700

    refactor: :lipstick: image gallery

--- resources/views/product-detail.blade.php
+++ resources/views/product-detail.blade.php
@@ -441,5 +441,18 @@
             slideMargin:0
             });
         });
+
+        $(document).ready(function() {
+            $('#vertical').lightSlider({
+            gallery:true,
+            item:1,
+            vertical:true,
+            verticalHeight:295,
+            vThumbWidth:50,
+            thumbItem:8,
+            thumbMargin:4,
+            slideMargin:0
+            });
+        });
     </script>
 @endpush

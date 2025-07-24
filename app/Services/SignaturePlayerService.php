e5699e20b7a21730215f004cf600ab2e5b4df8fe
akmlrnyn <akmalranyan@gmail.com>
2025-07-25 00:09:46 +0700

    feat(migration): add column to signature player

--- app/Services/SignaturePlayerService.php
+++ app/Services/SignaturePlayerService.php
@@ -6,12 +6,38 @@ use Illuminate\Http\Request;
 class SignaturePlayerService {
 	public function insertSignaturePlayer($request){
 		$data = $request->all();
+
+        $path = 'images/signature/'.$data['signature_code'];
+        $do_upload = imageUpload($data['image'], $path ,'public');
+
+        if(!$do_upload){
+            abort(500, 'Failed upload image');
+        } else {
+            $data['signature_image'] = $do_upload;
+        }
+
+        unset($data['image']);
+
         foreach ($data as $key => $value){ $signature[$key] = $value; }
         return $signature;
 	}
 
     public function updateSignaturePlayer($request){
         $data = $request->all();
+
+        if(isset($data['image'])) {
+            $path = 'images/signature/'.$data['signature_code'];
+            $do_upload = imageUpload($data['image'], $path ,'public');
+
+            if(!$do_upload){
+                abort(500, 'Failed upload image');
+            } else {
+                $data['signature_image'] = $do_upload;
+            }
+
+            unset($data['image']);
+        }
+
         foreach ($data as $key => $value) { $signature[$key] = $value; }
         return $signature;
     }

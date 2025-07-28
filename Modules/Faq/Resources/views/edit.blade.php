8651073d1d795818c1140106de73e2278692725d
akmlrnyn <akmalranyan@gmail.com>
2025-07-28 16:16:30 +0700

    refactor(view): view adjusment

--- Modules/Faq/Resources/views/edit.blade.php
+++ Modules/Faq/Resources/views/edit.blade.php
@@ -11,7 +11,7 @@
                 @csrf
                 @method('PUT')
 
-                @include('faq::_partials._form', ['faq' => $faq, 'edit' => false])
+                @include('faq::_partials._form', ['faq' => $faq, 'edit' => true])
 
                 <div class="text-right">
                     <button type="submit" class="btn btn-primary" id="form-submit">

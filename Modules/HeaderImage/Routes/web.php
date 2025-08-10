ae4f568ee6d2cb32a1f420c30be18ddc2abfb158
akmlrnyn <akmalranyan@gmail.com>
2025-08-10 11:40:13 +0700

    feat(route): header-image routes

--- Modules/HeaderImage/Routes/web.php
+++ Modules/HeaderImage/Routes/web.php
@@ -11,6 +11,18 @@
 |
 */
 
-Route::prefix('headerimage')->group(function() {
-    Route::get('/', 'HeaderImageController@index');
+use Modules\HeaderImage\Http\Controllers\HeaderImageController;
+
+Route::prefix('administrator/master-data/header-image')
+    ->name('administrator.master-data.header-image.')
+    ->middleware('auth')
+    ->group(function () {
+        Route::get('/', [HeaderImageController::class, 'index'])->name('index');
+        Route::get('/create', [HeaderImageController::class, 'create'])->name('create');
+        Route::get('/show/{id}', [HeaderImageController::class, 'show'])->name('show');
+        Route::get('/edit-{id}', [HeaderImageController::class, 'edit'])->name('edit');
+        Route::delete('/destroy/{id}', [HeaderImageController::class, 'destroy'])->name('destroy');
+        Route::post('/store', [HeaderImageController::class, 'store'])->name('store');
+        Route::put('/update/{id}', [HeaderImageController::class, 'update'])->name('update');
 });
+

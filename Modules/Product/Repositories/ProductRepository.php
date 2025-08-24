5be75225eae8982ca97a6695a10067752ff3ccdc
fernandes-wiraharjo <fernandeswiraharjo@gmail.com>
2025-08-24 17:44:13 +0700

    fix group by product

--- Modules/Product/Repositories/ProductRepository.php
+++ Modules/Product/Repositories/ProductRepository.php
@@ -107,7 +107,23 @@ class ProductRepository extends Repository implements MasterRepositoryInterface 
                 ->whereColumn('product_details.product_id', 'products.id')
                 ->where('product_details.qty', '<>', 0);
             })
-            ->groupBy('products.id');
+            ->groupBy(
+                'products.id',
+                'products.product_code',
+                'products.product_name',
+                'products.product_link',
+                'products.shopee_link',
+                'products.tiktok_link',
+                'products.blibli_link',
+                'products.description',
+                'products.image',
+                'products.product_visit',
+                'products.is_active',
+                'products.created_at',
+                'products.updated_at',
+                'pd.min_retail_price',
+                'pd.min_after_discount_price'
+            );
 
         return $q;
     }

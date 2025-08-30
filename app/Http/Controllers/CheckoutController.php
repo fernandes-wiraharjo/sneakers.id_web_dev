e1e3e440b1c4f7a1eacb4adadf5c464eb6e825f4
akmlrnyn <akmalranyan@gmail.com>
2025-08-30 17:30:49 +0700

    refactor(checkout-payment): SNAP link generate and hit API payment status check

--- app/Http/Controllers/CheckoutController.php
+++ app/Http/Controllers/CheckoutController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Services\MidtransService;
 use Illuminate\Routing\Controller as BaseController;
 use Illuminate\Support\Facades\View;
 use Illuminate\Http\Request;
@@ -10,6 +11,8 @@ use App\Facades\CheckoutXendit as Service;
 use App\Facades\Cart as CartService;
 use App\Facades\Transaction as TransactionService;
 use Illuminate\Support\Facades\Mail;
+use Midtrans\Config;
+use Midtrans\Transaction as MidtransTransaction;
 use Modules\Transaction\Entities\Transaction;
 
 class CheckoutController extends BaseController {
@@ -28,38 +31,38 @@ class CheckoutController extends BaseController {
 
         try {
             $transaction = Transaction::where('token', $external_id)->first();
-            $data['response'] = \Xendit\Invoice::retrieve($transaction->doc_no);
+            $data['response'] = MidtransTransaction::status($external_id);
             $data['transaction'] = $transaction;
             $data['items'] = $transaction->items()->with('detail', 'detail.product')->get();
             $data['shipping'] = $transaction->shipping()->first();
             $data['destination'] = $transaction->destination()->with('region')->first();
             //check status transaction dgn respose jika status != respose status maka update method, type, status
 
-            if($transaction->status != $data['response']['status']){
-                $email = $data['response']['customer']['email'];
-                $data_email = [
-                    'order_url' => $data['response']['success_redirect_url'],
-                    'customer_name' => $data['response']['customer']['given_names']." ".$data['response']['customer']['surname'],
-                    'order_id' => $data['response']['external_id']
+            if($transaction->status != data_get($data, 'response.transaction_status')){
+                $user = $transaction->user;
+                 $data_email = [
+                    'order_url' => '',
+                    'customer_name' => $user->first_name . " " . $user->last_name,
+                    'order_id' => data_get($data, 'response.order_id'),
                 ];
 
-                $sendMail = Mail::send('email.success-email', $data_email , function($message) use($email){
-                    $message->to($email);
+                Mail::send('email.success-email', $data_email, function ($message) use ($user) {
+                    $message->to($user->email);
                     $message->subject('SNEAKERS.ID Order Confirmed.');
                 });
 
 
                 $updateTransaction = Transaction::where('id', $transaction->id)->first();
                 $updateTransaction->update([
-                    'type' => $data['response']['payment_channel'],
-                    'method' => $data['response']['payment_method'],
-                    'status' => $data['response']['status'],
+                    'type' => data_get($data, 'response.payment_type'),
+                    'method' => data_get($data, 'response.bank'),
+                    'status' => data_get($data, 'response.transaction_status'),
                 ]);
 
                 TransactionService::insertHistories([
                     'transaction_id' => $transaction->id,
                     'response_raw' => $data['response'],
-                    'response_status' => $data['response']['status'],
+                    'response_status' => data_get($data, 'response.transaction_status'),
                     'response_code' => 200,
                     'response_message' => 'Success Payment from redirect page.',
                 ]);
--- app/Services/CheckoutService.php
+++ app/Services/CheckoutService.php
@@ -4,62 +4,122 @@ namespace App\Services;
 
 use Xendit\Xendit;
 use App\Facades\Transaction;
+use Midtrans\Config;
+use Midtrans\Snap;
 
 class CheckoutService {
 
     function __construct() {
-        Xendit::setApiKey(env('API_KEY'));
+        // Xendit::setApiKey(env('API_KEY'));
+        Config::$serverKey    = config('services.midtrans.serverKey');
+        Config::$clientKey    = config('services.midtrans.clientKey');
+        Config::$isProduction = ! in_array(config('app.env'), ['local', 'uat']);
+        Config::$isSanitized  = true;
+        Config::$is3ds        = true;
     }
 
-    public function createInvoice($args, $transactions) {
-        $date = new \DateTime();
-        $redirectUrl = '';
-        $defParams = [
-            'external_id' => 'sneakers-id-payments-' . $date->getTimestamp(),
-            'payer_email' => 'info@sneakers.id',
-            'description' => 'Sneakers Invoice payment.',
-            'customer_notification_preference' => [
-            //     'invoice_created' => [
-            //         'whatsapp'
-            //     ],
-            //     'invoice_reminder' => [
-            //         'whatsapp'
-            //     ],
-            //     'invoice_paid' => [
-            //         'whatsapp'
-            //     ],
-                'invoice_expired' => [
-                    // 'whatsapp',
-                    'email'
-                ]
-            ],
-            'failure_redirect_url' => $redirectUrl,
-            'success_redirect_url' => $redirectUrl
-        ];
-
-        $data = json_decode(json_encode($args), true);
-        $defParams['failure_redirect_url'] = $data['error_redirect_url'];
-        $defParams['success_redirect_url'] = route('customer.payment.success', $defParams['external_id']);
-        $defParams['customer'] = $data['customer'];
-        $defParams['items'] = $data['items'];
-        $defParams['fees'] = $data['fees'];
-
-        $params = array_merge($defParams, $data);
+    public function createInvoiceMidtrans($args, $transactions)
+    {
         $response = [];
 
         try {
-            $response = \Xendit\Invoice::create($params);
-            //insert into transactions -> $transaction
-            Transaction::createTransaction($response, $transactions);
-        } catch (\Throwable $e) {
-            $response['message'] = $e->getMessage();
+            if (is_string($args)) {
+                $args = json_decode($args, true);
+            }
 
-            // if($response['code' != 200]) {
-            //     //log error to slack and logger
-            // }
+            if (!is_array($args)) {
+                throw new \Exception("Args is not valid array, got: " . gettype($args));
+            }
+
+            if (!isset($args['transaction_details']['order_id'])) {
+                throw new \Exception("Missing order_id in args['transaction_details']");
+            }
+
+            $snapToken = Snap::getSnapToken($args);
+
+            // $statusResponse = MidtransTransaction::status($args['transaction_details']['order_id']);
+
+            //  dd(
+            //     [
+            //         "snap" => $snapToken,
+            //         "args" => $args['transaction_details']['order_id'],
+            //         "stats" => $statusResponse,
+            //     ]
+            // );
+
+            $transactionData = [
+                'args' => $args,
+                // 'status_response' => $statusResponse,
+                'invoice_url' => "https://app.sandbox.midtrans.com/snap/v2/vtweb/" . $snapToken,
+            ];
+
+            Transaction::createTransaction($transactionData, $transactions);
+
+            $response = $transactionData;
+        } catch (\Throwable $e) {
+            dd([
+                "datas" => $args,
+                "e" => $e,
+                "transaction" => $transactions,
+            ]);
+            $response['message'] = $e->getMessage();
         }
 
         logger($response);
         return $response;
     }
+
+
+    // public function createInvoiceXendit($args, $transactions) {
+    //     $date = new \DateTime();
+    //     $redirectUrl = '';
+    //     $defParams = [
+    //         'external_id' => 'sneakers-id-payments-' . $date->getTimestamp(),
+    //         'payer_email' => 'info@sneakers.id',
+    //         'description' => 'Sneakers Invoice payment.',
+    //         'customer_notification_preference' => [
+    //         //     'invoice_created' => [
+    //         //         'whatsapp'
+    //         //     ],
+    //         //     'invoice_reminder' => [
+    //         //         'whatsapp'
+    //         //     ],
+    //         //     'invoice_paid' => [
+    //         //         'whatsapp'
+    //         //     ],
+    //             'invoice_expired' => [
+    //                 // 'whatsapp',
+    //                 'email'
+    //             ]
+    //         ],
+    //         'failure_redirect_url' => $redirectUrl,
+    //         'success_redirect_url' => $redirectUrl
+    //     ];
+
+    //     $data = json_decode(json_encode($args), true);
+    //     $defParams['failure_redirect_url'] = $data['error_redirect_url'];
+    //     $defParams['success_redirect_url'] = route('customer.payment.success', $defParams['external_id']);
+    //     $defParams['customer'] = $data['customer'];
+    //     $defParams['items'] = $data['items'];
+    //     $defParams['fees'] = $data['fees'];
+
+    //     $params = array_merge($defParams, $data);
+    //     $response = [];
+
+    //     try {
+    //         $response = \Xendit\Invoice::create($params);
+    //         //insert into transactions -> $transaction
+    //         Transaction::createTransaction($response, $transactions);
+    //     } catch (\Throwable $e) {
+    //         $response['message'] = $e->getMessage();
+
+    //         // if($response['code' != 200]) {
+    //         //     //log error to slack and logger
+    //         // }
+    //     }
+
+    //     logger($response);
+    //     return $response;
+    // }
+
 }

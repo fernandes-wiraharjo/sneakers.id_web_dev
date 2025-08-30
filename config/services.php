84ada40d3098472523f12365afa640db9ad037ba
akmlrnyn <akmalranyan@gmail.com>
2025-08-30 17:26:28 +0700

    feat(checkout-payment): midtrans configs

--- config/services.php
+++ config/services.php
@@ -30,4 +30,8 @@ return [
         'region' => env('AWS_DEFAULT_REGION', 'us-east-1'),
     ],
 
+    'midtrans' => [
+        'serverKey' => env('MIDTRANS_SERVER_KEY'),
+        'clientKey' => env('MIDTRANS_CLIENT_KEY'),
+    ],
 ];

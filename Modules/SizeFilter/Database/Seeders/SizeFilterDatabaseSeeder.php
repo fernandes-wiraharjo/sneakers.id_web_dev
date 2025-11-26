9503ca62f99d92a97203d60e52e686806d16e1ed
Nikko Agustino <nikkoagustino@gmail.com>
2025-11-26 23:07:15 +0700

    feature: add size filter seeder

--- Modules/SizeFilter/Database/Seeders/SizeFilterDatabaseSeeder.php
+++ Modules/SizeFilter/Database/Seeders/SizeFilterDatabaseSeeder.php
@@ -16,6 +16,6 @@ class SizeFilterDatabaseSeeder extends Seeder
     {
         Model::unguard();
 
-        // $this->call("OthersTableSeeder");
+        $this->call(SizeFilterSeeder::class);
     }
 }

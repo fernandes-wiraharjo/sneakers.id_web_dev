0898e6421c064beab6acd0da09e0ba0ebccfd63b
Nikko Agustino <nikkoagustino@gmail.com>
2026-01-25 08:28:35 +0700

    fix: remove unnecessary max-height style

--- resources/views/bootstrap/parts/navbar.blade.php
+++ resources/views/bootstrap/parts/navbar.blade.php
@@ -5,7 +5,7 @@
     <div class="container">
         <!-- Brand/Logo -->
         <a class="navbar-brand order-0 me-auto" href="{{ route('store') }}">
-            <img src="{{ $logo_navbar }}" alt="SNEAKERS.ID" class="img-fluid" style="max-height: 70px;">
+            <img src="{{ $logo_navbar }}" alt="SNEAKERS.ID" class="img-fluid">
         </a>
 
         <!-- Mobile Toggle Button -->

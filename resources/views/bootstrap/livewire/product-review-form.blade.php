ba6721876c7ffe99365fc1daa314a1a9177f6546
Nikko Agustino <nikkoagustino@gmail.com>
2026-01-29 22:23:47 +0700

    fix: review star blinking (livewire)

--- resources/views/bootstrap/livewire/product-review-form.blade.php
+++ resources/views/bootstrap/livewire/product-review-form.blade.php
@@ -7,13 +7,14 @@
     @endif
 
     <div class="mb-3">
-        <div class="d-flex gap-2">
+        <input type="hidden" id="rating-input-{{ $productId }}-{{ $productSize }}" value="{{ $rating }}">
+        <div class="d-flex gap-2 rating-stars" data-form-id="{{ $productId }}-{{ $productSize }}">
             @for ($i = 1; $i <= 5; $i++)
                 <button 
                     type="button" 
                     wire:key="rating-{{ $i }}"
-                    class="btn btn-link p-0 border-0 {{ $rating >= $i ? 'text-warning' : 'text-secondary' }}"
-                    wire:click="setRating({{ $i }})"
+                    class="btn btn-link p-0 border-0 rating-star {{ $rating >= $i ? 'text-warning' : 'text-secondary' }}"
+                    data-rating="{{ $i }}"
                     @if($isSubmitted) disabled @endif
                     style="font-size: 1.5rem; text-decoration: none;">
                     <span class="iconify" data-icon="material-symbols:star"></span>
@@ -37,7 +38,7 @@
     <button 
         type="button" 
         class="btn btn-dark w-100"
-        wire:click="submitReview"
+        id="submit-review-{{ $productId }}-{{ $productSize }}"
         @if($isSubmitted) disabled @endif>
         @if($isSubmitted)
             <span class="iconify me-2" data-icon="material-symbols:check-circle"></span>
@@ -47,4 +48,40 @@
         @endif
     </button>
 </div>
+
+@push('scripts')
+<script>
+    $(document).ready(function() {
+        var formId = '{{ $productId }}-{{ $productSize }}';
+        var ratingInput = $('#rating-input-' + formId);
+        var stars = $('.rating-stars[data-form-id="' + formId + '"] .rating-star');
+        var submitButton = $('button[wire\\:click="submitReview"]');
+
+        stars.on('click', function() {
+            if ($(this).prop('disabled')) {
+                return;
+            }
+
+            var selectedRating = parseInt($(this).data('rating'));
+            ratingInput.val(selectedRating);
+            
+            stars.each(function(index) {
+                var starRating = index + 1;
+                if (starRating <= selectedRating) {
+                    $(this).removeClass('text-secondary').addClass('text-warning');
+                } else {
+                    $(this).removeClass('text-warning').addClass('text-secondary');
+                }
+            });
+        });
+
+        // Handle submit button click
+        $('#submit-review-' + formId).on('click', function() {
+            var rating = parseInt(ratingInput.val()) || 0;
+            @this.set('rating', rating);
+            @this.call('submitReview');
+        });
+    });
+</script>
+@endpush
 
